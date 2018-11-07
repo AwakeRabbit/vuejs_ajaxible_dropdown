5a0c16d8961b4cd2e3b30c2486c35c1042474e07
Egor Vasilenko <y.vasilenko@investars.com>
2018-11-08 01:19:37 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <br>
+    <label for="">Editable: </label>
+    <input type="checkbox" v-model="editable">
+    <input type="text" v-model="current" placeholder="Add new item" @keyup.enter="addItem"/>
+    <br><br><br>
+    <Dropdown :data="items" v-model="val" :editable="editable" :on-key-pause="(str) => {this.items = []; for (let i = 0; i < 10; i++) items.push({Value: i, Text: str + i})}"/>
+    <br>  <br><br>
+    <br>
+    <label for="">Choosed: </label><p>"{{val ? items.filter(i => i.Value === val)[0] ? items.filter(i => i.Value === val)[0].Text : '' : ''}}"</p>
+  </div>
+</template>
+
+<script>
+import Dropdown from './components/Dropdown.vue'
+
+export default {
+  name: "app",
+  data: () => ({
+    current: "",
+    items: [],
+    val: null,
+    editable: false,
+    console: window.console
+  }),
+  components: {
+    Dropdown
+  },
+  methods: {
+    addItem: function() {
+      this.items.push({Text: this.current, Value: this.items.length});
+      this.current = "";
+    },
+    alert: function(text) {
+      window.alert(text)
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="dropdown" :class="{expanded, loading}">
+    <input type="text"  :readonly="!editable || !(expanded && editable)" v-on:click="showList" @keyup="checkList" ref="inp" />
+    <div class="fake-background" v-on:click="expanded = false"></div>
+      <ul>
+        <li v-for="i in items.filter(i => i.hidden !== true)" v-on:click="setValue(i.Value)" v-bind:key="i.Value" v-if="items.filter(i => i.hidden !== true).length > 0">
+            
+            {{i.Text}}
+        </li>
+        <li class="empty" v-if="items.length == 0 || items.filter(i => i.hidden !== true).length == 0">No data</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dropdown',
+  props: {
+    data: {
+      type: Array,
+      default: []
+    },
+    editable: {
+      type: Boolean,
+      default: false
+    },
+    timeout: {
+      type: Number,
+      default: 3000
+    },
+    onKeyPause: {
+      type: Function,
+      default: null
+    }
+
+  },
+  data: () => ({ 
+    Text: '', 
+    Value: null, 
+    expanded: false, 
+    loading: false, 
+    items: [], 
+    lastKeyUp: false 
+  }),
+
+  methods: {
+    setValue: function(val) {
+      this.expanded = false;
+      let item = this.items.filter(i => i.Value === val)[0]
+      if (!item) return
+      this.Value = item.Value 
+      this.Text = item.Text
+      this.$refs.inp.value = this.Text
+      this.$emit("input", this.Value)
+    },
+    checkList: function(event) {
+      if (this.onKeyPause != null) {
+        if (event.keyCode == 13) {
+            this.loading = true
+            this.onKeyPause(this.$refs.inp.value.trim())
+            this.lastKeyUp = 0
+        } else {
+            this.lastKeyUp = new Date()
+            setTimeout(() => {
+              if (this.expanded) { 
+                if (new Date() - this.lastKeyUp >= this.timeout && this.lastKeyUp != 0) {
+                    this.loading = true
+                    this.onKeyPause(this.$refs.inp.value.trim())
+                    this.lastKeyUp = 0
+                  }
+              } else {
+                this.lastKeyUp = 0
+              }
+            } , 3000)
+        }
+        
+      }
+      this.items = this.items.map( i =>  ({...i, hidden: (i.Text.indexOf(this.$refs.inp.value.trim()) < 0)}))
+    },
+    showList: function() {
+        if (!this.expanded) this.items = this.items.map(i => ({...i, hidden: false}))
+        this.expanded = true
+    }
+  },
+  mounted: function () {
+    window.addEventListener('keyup', function(e) {
+        if (e.keyCode == 27) {
+            let dd =  document.querySelector('.dropdown.expanded .fake-background') 
+            if (dd) {
+               dd.click()
+            }
+        }
+    }) 
+  },
+  watch: {
+    data: function(newVal, oldVal) {
+      this.loading = false
+      this.items = newVal || []
+  }}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+$green: #42b983;
+a {
+  color: $green;
+}
+.dropdown {
+  position: relative;
+  display: inline-block;
+  input {
+    border: 1px solid #42b983;
+    border-width: 0 0 1px;
+    padding: 5px;
+    cursor: pointer;
+    user-select: none;
+  }
+  .fake-background {
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba($color: #000000, $alpha: 0);
+    z-index: 9999;
+  }
+  ul {
+    position: absolute;
+    top: 26px;
+    left: 0;
+    display: none;
+    z-index: 10000;
+    background: #fff;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+    min-width: 100%;
+    text-align: left;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    overflow-y: auto;
+    max-height: 200px;
+    user-select: none;
+  }
+  li {
+    cursor: pointer;
+    padding: 8px 10px;
+    max-width: 200px;
+    font-size: 13px;
+    line-height: 1em;
+    word-wrap: break-word;
+    &:hover {
+      background: lighten($color: $green, $amount: 40%);
+    }
+
+    &.empty {
+      color: #999;
+    }
+  }
+  &.expanded {
+    input {
+      z-index: 10000;
+    }
+    .fake-background,
+    ul {
+      display: block;
+    }
+  }
+}
+</style>
